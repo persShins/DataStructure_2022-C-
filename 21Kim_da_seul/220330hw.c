3d3dbe08b33104ab1f95b8bb9f7f15dc54006038
d4m0n9 <0x57415645@gmail.com>
2022-03-31 14:58:04 +0900

    220330 hw

--- 21Kim_da_seul/220330hw.c
+++ 21Kim_da_seul/220330hw.c
@@ -1,3 +1,6 @@
+#pragma warning (disable:4996)
+#pragma warning (disable:6031)
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -19,7 +22,7 @@ int main()
 void change(char *s1, int num)
 {
 	char temp;
-	int size = strlen(s1);
+       	int size = strlen(s1);
 	for (int i = 0; i < num; i++)
 	{
 		temp = s1[0];
@@ -28,5 +31,5 @@ void change(char *s1, int num)
 			s1[j] = s1[j + 1];
 		}
 		s1[size - 1] = temp;
-	}
+       	}
 }
