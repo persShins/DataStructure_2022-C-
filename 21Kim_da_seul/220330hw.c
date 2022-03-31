5f59ac7a1cdfc41ed5698a0001c7eade389f6c77
d4m0n9 <100832622+d4m0n9@users.noreply.github.com>
2022-03-31 14:47:23 +0900

    Update 220330hw.c

--- 21Kim_da_seul/220330hw.c
+++ 21Kim_da_seul/220330hw.c
@@ -5,30 +5,28 @@ void change(char *, int);
 
 int main()
 {
-	    int rep;
-	        char *s1 = malloc(sizeof(char) * 30);
-		    scanf(" %s", s1);
-		        scanf(" %d", &rep);
+	int rep;
+	char *s1 = malloc(sizeof(char) * 30);
+	scanf(" %s", s1);
+	scanf(" %d", &rep);
 
-			    change(s1, rep);
-
-			        printf("%s", s1);
-
-				    free(s1);
+	change(s1, rep);
+	printf("%s", s1);
+	free(s1);
 
 }
 
 void change(char *s1, int num)
 {
-	    char temp;
-	        int size = strlen(s1);
-		    for (int i = 0; i < num; i++)
-			        {
-					        temp = s1[0];
-						        for (int j = 0; j < size - 1; j++)
-								        {
-										            s1[j] = s1[j + 1];
-											            }
-							        s1[size - 1] = temp;
-								    }
+	char temp;
+	int size = strlen(s1);
+	for (int i = 0; i < num; i++)
+	{
+		temp = s1[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			s1[j] = s1[j + 1];
+		}
+		s1[size - 1] = temp;
+	}
 }
